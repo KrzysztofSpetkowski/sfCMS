47c0b85047eec901f2e20377a7c09d8c9950523f
krzysiek <k.spetkowski@wp.pl>
2014-11-02 23:14:44 +0100

    confi AdminBundle

---
+++ src/Bootcamp/AdminBundle/Entity/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Bootcamp\AdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Page", mappedBy="user")
+     * @var ArrayCollection
+     */
+    protected $pages;
+
+    public function __construct()
+    {
+    	parent::__construct();
+    	
+    	$this->pages = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
